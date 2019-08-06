7161bf3fc05e4216851c9e84245ac028cf18fd7b
Mao Wtm <m@maowtm.org>
2019-08-06 21:22:45 +0800

    Better web interface

---
+++ web/assets/App.svelte
@@ -0,0 +1,55 @@
+<script>
+let check_state = JSON.parse(document.body.dataset.checkState);
+import CheckList from './CheckList.svelte';
+import { onMount, onDestroy } from 'svelte';
+let timeout_handle = null;
+let exited = false;
+const fetch_delay = 5000;
+onMount(() => {
+  timeout_handle = setTimeout(function g () {
+    timeout_handle = null;
+    fetch('/api/status_log', {method: 'GET', credentials: 'omit'}).then(res => {
+      if (res.status !== 200) {
+        return Promise.reject(res.status);
+      } else {
+        return res.json();
+      }
+    }).then(new_state => {
+      if (!exited) {
+        timeout_handle = setTimeout(g, fetch_delay);
+        check_state = new_state;
+      }
+    }, err => {
+      if (!exited) {
+        timeout_handle = setTimeout(g, fetch_delay);
+      }
+    })
+  }, fetch_delay)
+})
+onDestroy(() => {
+  if (timeout_handle != null) clearTimeout(timeout_handle);
+  exited = true;
+})
+</script>
+
+<style>
+  :global(:root) {
+    --color-green: #3CA83C;
+    --color-light-green: #C4E4C4;
+    --color-orange: #E57248;
+    --color-light-orange: #F8DBD1;
+    --color-red: #A83C3C;
+    --color-light-red: #FDC2C2;
+  }
+
+  :global(:root body) {
+    padding: 0;
+    background-color: white;
+    color: #444;
+    font-family: sans-serif;
+    box-sizing: border-box;
+    margin: 0 auto;
+  }
+</style>
+
+<CheckList checks={check_state.checks} />
---
+++ web/assets/CheckList.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let checks;
+  import CheckItem from './CheckItem.svelte';
+  let showing_more_desc = null;
+
+  function handle_toggle_more(desc) {
+    if (showing_more_desc == desc) {
+      showing_more_desc = null;
+    } else {
+      showing_more_desc = desc;
+    }
+  }
+
+</script>
+
+<style>
+  ul {
+    margin: 1rem;
+    padding: 0;
+  }
+</style>
+
+<ul>
+  {#each checks as check}
+    <CheckItem check={check} showing_more={showing_more_desc != null && check.desc == showing_more_desc}
+      on:togglemore="{e => handle_toggle_more(check.desc)}" />
+  {/each}
+</ul>
